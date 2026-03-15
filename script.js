c6038b13705d5f86355f61720881d9724abbba29
za264-create <za264@njit.edu>
2026-03-14 21:28:26 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showHeading() {
-    document.getElementById("myHeading").style.display = "block";
-
-}
-
-function hideHeading() {
-    document.getElementById("myHeading").style.display = "none";
-
-}
